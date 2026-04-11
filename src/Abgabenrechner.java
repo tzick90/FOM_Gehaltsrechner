840c15de6c7341c9d09bf23fb9e70f5b2df746d8
Torsten <torsten.zick@gmail.com>
2026-04-11 19:04:45 +0100

    added planning_comments for future calculations

---
+++ src/Abgabenrechner.java
@@ -0,0 +1,27 @@
+public class Abgabenrechner {
+    //Konstanten für Sozialversicherungsbeiträge (in Prozent)
+    // -> TODO: Definieren der Konstannten -> private static final double?
+    // -> Krankenversicherung Basis
+    // -> Rentenversicherung Basis
+    // -> Arbeitslosenversicherung
+    // -> Pflegeversicherung Standard
+    // -> PV- Kinderlos
+
+
+    // Methode zur Berchgnung der Krankenversicherung
+    // TODO: public double berechnungkrankenversicherung (double burttogehlt, double zusamtzbeitrag)
+    // Formel: bruttogehalt * (KRANKENVERSICHERUNG_BASIS + zusatzbeitrag) / 100
+
+
+    // Methode zur Berechnung der Rentenversicherung
+    // TODO: public double berechneRentenversicherung(double bruttogehalt)
+
+
+    // Methode zur Berechnung der Arbeitslosenversicherung
+    // TODO: public double berechneArbeitslosenversicherung(double bruttogehalt)
+
+
+    // Methode zur Berechnung der Pflegeversicherung
+    // TODO: public double berechnePflegeversicherung(double bruttogehalt, boolean kinderlos, int alter)
+    // // Wenn kinderlos UND alter > 23 → höherer Satz
+}
