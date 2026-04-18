2704d08c41a815a6f0ad0c62e8ca1bef6536a609
Torsten <torsten.zick@gmail.com>
2026-04-18 12:05:13 +0100

    New: added public methods to class Abgabenrechner

--- src/Abgabenrechner.java
+++ src/Abgabenrechner.java
@@ -1,27 +1,89 @@
 public class Abgabenrechner {
-    //Konstanten für Sozialversicherungsbeiträge (in Prozent)
-    // -> TODO: Definieren der Konstannten -> private static final double?
-    // -> Krankenversicherung Basis
-    // -> Rentenversicherung Basis
-    // -> Arbeitslosenversicherung
-    // -> Pflegeversicherung Standard
-    // -> PV- Kinderlos
 
 
-    // Methode zur Berchgnung der Krankenversicherung
-    // TODO: public double berechnungkrankenversicherung (double burttogehlt, double zusamtzbeitrag)
-    // Formel: bruttogehalt * (KRANKENVERSICHERUNG_BASIS + zusatzbeitrag) / 100
 
 
-    // Methode zur Berechnung der Rentenversicherung
-    // TODO: public double berechneRentenversicherung(double bruttogehalt)
+    /**
+    - Methode zur Berechnung der Krankenversicherung
+    - Arbeitnehmer zahl 50% von (Basis + Zusatzbeitrag)
+
+    */
+
+    public static double berechneKrankenversicherung(
+            double bruttogehalt,
+            double kvBasis,
+            double kvZusatz
+    ) {
+        // Arbeitnehmer zahlt nur die Hälfte (Arbeitgeber zahlt andere Hälfte)
+        return bruttogehalt * ((kvBasis + kvZusatz) / 2) / 100;
+    }
+
+    /**
+     *Berechnet die Rentenversicherung (Arbeitnehmeranteil)
+     */
+
+    public static double berechneRentenversicherung(
+            double bruttogehalt,
+            double rvSatz
+    ) {
+        return bruttogehalt * rvSatz / 100;
+    }
+
+    /**
+     * Berechnet die Arbeitslosenversicherung (AN-Anteil)
+     */
+
+    public static double berechneArbeitslosenversicherung(
+            double bruttogehalt,
+            double avSatz
+    ) {
+        return bruttogehalt * avSatz / 100;
+    }
+
+    /**
+     * Berechnet die Pflegeversicherung
+     * Achtung!: Höherer Satz für Kinderlose über 23 Jahre
+     */
+
+    public static double berechnePflegeversicherung(
+            double bruttogehalt,
+            double pvStandard,
+            double pvKinderlos,
+            boolean kinderlos,
+            int alter
+    ) {
+        // Kinderlose über 23 zahlen Zuschlag
+        double satz;
+        if (kinderlos && alter > 23) {
+            satz = pvKinderlos;
+        } else {
+            satz = pvStandard;
+        }
+
+        return bruttogehalt * satz / 100;
+    }
+
+    /**
+     * Berechnet Summe aller Sozialabgaben
+     */
+    /**public static double berechneGesamtSozialabgaben(
+            double bruttogehalt,
+            double kvBasis,
+            double kvZusatz,
+            double rvSatz,
+            double avSatz,
+            double pvStandard,
+            double pvKinderlos,
+            boolean kinderlos,
+            int alter
+    ) {
+        // TODO: Implementieren
+    }
+     */
 
 
-    // Methode zur Berechnung der Arbeitslosenversicherung
-    // TODO: public double berechneArbeitslosenversicherung(double bruttogehalt)
 
 
-    // Methode zur Berechnung der Pflegeversicherung
-    // TODO: public double berechnePflegeversicherung(double bruttogehalt, boolean kinderlos, int alter)
-    // // Wenn kinderlos UND alter > 23 → höherer Satz
+
+
 }
---
+++ src/GUI.java
@@ -0,0 +1,7 @@
+public class GUI {
+}
+
+Auto a1 = new Auto();
+    a1.farbe = "gruen";
+    a1.ps = "farbe";
+    a1.hersteller = "audi";
